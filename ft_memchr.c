7de27df391c2589ba6f45350a3eb150e45c32131
diazmanne <enteyedo@hotmail.com>
2019-03-04 19:15:08 -0800

    update,memchr

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emdiaz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/04 04:40:40 by emdiaz            #+#    #+#             */
+/*   Updated: 2019/03/04 17:56:30 by emdiaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+		{
+			return ((void *)(&str[i]));
+			i++;
+		}
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emdiaz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 00:28:09 by emdiaz            #+#    #+#             */
-/*   Updated: 2019/03/04 04:39:57 by emdiaz           ###   ########.fr       */
+/*   Updated: 2019/03/04 19:03:53 by emdiaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,6 @@ void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void s2, size_t n);
 #endif
