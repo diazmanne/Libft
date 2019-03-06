4ba5b420e7a98f854d041da74aa2b097cc601508
diazmanne <enteyedo@hotmail.com>
2019-03-06 03:38:47 -0800

    86 lines max error

--- libft.h
+++ libft.h
@@ -6,16 +6,15 @@
 /*   By: emdiaz <emdiaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 00:28:09 by emdiaz            #+#    #+#             */
-/*   Updated: 2019/03/06 02:45:15 by emdiaz           ###   ########.fr       */
+/*   Updated: 2019/03/06 03:36:14 by emdiaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+# define LIBFT_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 int				ft_toupper(int c);
 int				ft_tolower(int c);
@@ -34,9 +33,11 @@ char			*ft_strncpy(char *dst, const char *src, size_t len);
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strstr(const char *haystack, const char *needle);
-char			*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char			*ft_strnstr(const char *haystack,
+								const char *needle, size_t len);
 char			*ft_strcat(char *s1, const char *s2);
-char			*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+char			*ft_strncat(char *restrict s1,
+								const char *restrict s2, size_t n);
 size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
 char			*ft_strdup(const char *s1);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
